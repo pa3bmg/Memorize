93243c68edc0567dd4d130dd70744f91d212a7bb
Ton Smink <pa3bmg@home.nl>
2024-02-19 22:07:24 +0100

    Make it lean

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,20 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻","👹","👺","☠️","🤖","👽","💀"]
+    
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            cards
+            cardCountAdjusters
         }
-        .foregroundColor(.orange)
         .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            addButton
+            Spacer()
+            removeButton
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .padding()
+    }
+    
+    var cards: some View {
+        HStack {
+            ForEach(0..<cardCount , id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }.foregroundColor(.orange)
+    }
+    
+    var addButton: some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label:{
+            Image(systemName: "bag.badge.plus.fill")
+        })
+    }
+    
+    var removeButton: some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        }, label:{
+            Image(systemName: "bag.badge.minus.fill")
+        })
     }
 }
 
 struct CardView: View {
-    @State var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack(content:  {
@@ -29,7 +70,7 @@ struct CardView: View {
             if (isFaceUp){
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
